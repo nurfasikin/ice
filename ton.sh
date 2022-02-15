f7152067a31f743d5f43e65d4ace695bb6f59e40
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-15 17:36:26 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -5,5 +5,5 @@ nohup sudo apt-get install libpci3
 nohup wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.43/lolMiner_v1.43_Lin64.tar.gz 
 nohup tar -xvf lolMiner_v1.41b_Lin64.tar.gz
 cd 1.43 
-mv lolMiner izzana
-screen -d -m ./izzana --algo TON --pool ton.hashrate.to:4003 --user EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI.cepetsugih
+mv lolMiner
+screen -d -m ./lolMiner --algo TON --pool ton.hashrate.to:4003 --user EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI.cepetsugih
